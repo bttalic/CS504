84bfafa65bda94648cf18a7dfc7785d4d3c69a92
bttalic <b.ttalic@gmail.com>
2015-05-10 19:28:56 +0200

    added css minify, navigation, template

--- Frontend/Scripts/app.js
+++ Frontend/Scripts/app.js
@@ -1,6 +1,54 @@
 (function(){
 	
-	var app = angular.module("TodoApp", []);
+	//initialize app and dependencies
+	var app = angular.module("TodoApp", ['ngRoute']);
+    
+    app.controller("LoginController", ["$scope", function($scope){
+        $scope.user = {};
+        
+        $scope.message = "Hello from login";
+        
+        $scope.login = function(){
+          console.log($scope.user);  
+        };
+    }]);
+    
+    app.controller("NavigationController", ["$scope", "$location", function($scope, $location){
+       
+       $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+       
+    }]);
+    
+    app.controller("HomeController", ["$scope", function($scope){
+        
+    }]);
+	
+	//configure angular routes
+	app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
+        $routeProvider
+
+            .when("/", {
+              templateUrl: "./templates/home.html",
+              controller: "HomeController"  
+            })
+            // route for the login page
+            .when('/login', {
+                templateUrl : './templates/login.html',
+                controller  : 'LoginController'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : './templates/about.html'
+            })
+            .otherwise({ redirectTo: '/' });
+            
+            //to make the routes user friendly
+            $locationProvider.html5Mode(true);
+    }]);
+	
 	
 	
 	
--- Frontend/gruntfile.js
+++ Frontend/gruntfile.js
@@ -10,12 +10,23 @@ module.exports = function (grunt) {
             }
         },
         watch: {
-            files: ["Scripts/*.js"],
-            tasks: ["uglify"]
+            files: ["Scripts/*.js", "Stylesheets/*.css"],
+            tasks: ["uglify", "cssmin"]
         },
         uglify: {
+            scripts: {
+                files: { "./app.js": ["./Scripts/**/*.js"] }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
             target: {
-                files: { "./app.js": "Scripts/app.js" }
+                files: {
+                    'style.css': ["./Stylesheets/**/*.css"]
+                }
             }
         }
     });
@@ -24,10 +35,12 @@ module.exports = function (grunt) {
     grunt.registerTask("default", ["watch"]);
     grunt.registerTask("bower", ["bower:install"]);
     grunt.registerTask("uglify", ["uglify"]);
+    grunt.registerTask("cssmin", ["cssmin"]);
 
     // The following line loads the grunt plugins.
     // This line needs to be at the end of this this file.
     grunt.loadNpmTasks("grunt-bower-task");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 };
